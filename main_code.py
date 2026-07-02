771027c403df6f55845284f52751d7dc9d6cf1cc
Aidana <aidana.street@gmail.com>
2026-07-02 19:52:09 +0500

    started

---
+++ main_code.py
@@ -0,0 +1,13 @@
+sales = [1200, 850, 430, 2100, 1750]
+
+print(sales)
+
+def calculate_total(sales):
+    total = 0
+    for i in sales:
+        total += i
+
+    return total
+    
+res = calculate_total(sales)
+print(res)
