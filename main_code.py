b5a24373ab48943e8d3cd79f34b949529eb4aca1
darika <darikarysmendeeva@gmail.com>
2026-07-03 15:53:32 +0500

    добавила функциия, которые находят средную продажу и максимальную продажу

--- main_code.py
+++ main_code.py
@@ -11,3 +11,20 @@ def calculate_total(sales):
     
 res = calculate_total(sales)
 print(res)
+
+
+def avarage_value(sales):
+    avarage_sale = sum(sales)/len(sales)
+    return avarage_sale
+avarage = avarage_value(sales)
+print(f'Средняя продажа:{avarage}')
+
+def max_sale(sales):
+    max1 = max(sales)
+    return max1
+maximum = max_sale(sales)
+print(f'Максимальная продажа:{maximum}')
+
+
+    
+
