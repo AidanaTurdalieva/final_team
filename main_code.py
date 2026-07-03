06d00477950776be9648e6941f4bbd665cb499bc
Aidana <aidana.street@gmail.com>
2026-07-03 15:39:08 +0500

    new functions added

--- main_code.py
+++ main_code.py
@@ -13,7 +13,7 @@ res = calculate_total(sales)
 print(res)
 
 def find_min_sale(sales):
-    min_sale = 0
+    min_sale = sales[0]
     for i in sales:
         if i < min_sale:
             min_sale = i
@@ -21,4 +21,10 @@ def find_min_sale(sales):
     return min_sale
 
 min_sale = find_min_sale(sales)
-print(min_sale)   
+print(min_sale)  
+
+def count_sales(sales):
+    return len(sales)
+
+sales_count = count_sales(sales)
+print(sales_count)
